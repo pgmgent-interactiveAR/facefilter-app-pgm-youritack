36b135036ac0dbdb792617a0f5138c09c232f3b1
pgm-youritack <youri.tack@student.arteveldehs.be>
2022-12-04 13:35:27 +0100

    added christmas objects & simple christmas design

--- scripts/app.js
+++ scripts/app.js
@@ -1,21 +1,64 @@
-const app={
-    init(){
-        this.cacheElements();
-        this.eventListeners();
-    },
-    cacheElements(){
-        this.$splash = document.querySelector('.splash');
+const app = {
+  init() {
+    this.cacheElements();
+    this.eventListeners();
+  },
+  cacheElements() {
+    this.$splash = document.querySelector(".splash");
 
-    },
-    eventListeners(){
-        this.$splash.addEventListener("click",this.hideSplashscreen,false)
-    },
-    hideSplashscreen(){
-        console.log(this);
-        this.style.opacity=0;
-        setTimeout(()=>{
-            this.classList.add('hidden')
-          },610)
-    },
-}
+    this.$halocheck = document.querySelector("#haloCheck");
+    this.$santaHatCheck = document.querySelector("#santaHatCheck");
+    this.$christmasBellCheck = document.querySelector("#christmasBellCheck");
+    this.$christmasBallCheck = document.querySelector("#christmasBallCheck");
+
+    this.$santaHatEntity = document.querySelector("#santaHatEntity");
+    this.$christmasBellEntity = document.querySelector("#christmasBellEntity");
+    this.$christmasBallEntityLeft = document.querySelector("#christmasBallEntityLeft");
+    this.$christmasBallEntityRight = document.querySelector("#christmasBallEntityRight");
+    this.$haloEntity = document.querySelector("#haloEntity");
+  },
+  eventListeners() {
+    this.$splash.addEventListener("click", this.hideSplashscreen, false);
+    this.$halocheck.addEventListener("change", this.showHalo.bind(this), false);
+    this.$santaHatCheck.addEventListener("change",this.showSantaHat.bind(this),false);
+        this.$christmasBallCheck.addEventListener("change", this.showChristmasBall.bind(this), false);
+    this.$christmasBellCheck.addEventListener("change",this.showChristmasBell.bind(this),false);
+  },
+  hideSplashscreen() {
+    this.style.opacity = 0;
+    setTimeout(() => {
+      this.classList.add("hidden");
+    }, 610);
+  },
+  showHalo() {
+    if (this.$halocheck.checked) {
+      this.$haloEntity.setAttribute("visible", true);
+    } else {
+      this.$haloEntity.setAttribute("visible", false);
+    }
+  },
+  showSantaHat() {
+    if (this.$santaHatCheck.checked) {
+      this.$santaHatEntity.setAttribute("visible", true);
+    } else {
+      this.$santaHatEntity.setAttribute("visible", false);
+    }
+  },
+  showChristmasBell() {
+    if (this.$christmasBellCheck.checked) {
+      this.$christmasBellEntity.setAttribute("visible", true);
+    } else {
+      this.$christmasBellEntity.setAttribute("visible", false);
+    }
+  },
+  showChristmasBall() {
+    if (this.$christmasBallCheck.checked) {
+      this.$christmasBallEntityLeft.setAttribute("visible", true);
+      this.$christmasBallEntityRight.setAttribute("visible", true);
+    } else {
+      this.$christmasBallEntityLeft.setAttribute("visible", false);
+      this.$christmasBallEntityRight.setAttribute("visible", false);
+    }
+  },
+};
 app.init();
